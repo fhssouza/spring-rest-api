711a156ae67c3c3df73246d574756a25b90b83f1
fhssouza <fhssouza@gmail.com>
2022-12-19 16:59:42 -0300

    Retornando usuário em JSON para a tela

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import curso.api.rest.model.Usuario;
+
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
@@ -15,6 +17,19 @@ public class IndexController {
 	public ResponseEntity<String> init(@RequestParam (value = "nome", required = true, defaultValue = "Nome não informado") String nome, @RequestParam("salario") long salario) {
 		System.out.println("Parametro sendo recebido " + nome + " Salario é: " + salario);
 		return new ResponseEntity<String>("Olá Usuário REST Spring Boot seu nome é " + nome + " Salario é: " + salario, HttpStatus.OK);
+	}
+	
+	@GetMapping
+	public ResponseEntity<Usuario> usuario(){
+		
+		Usuario usuario = new Usuario();
+		usuario.setId(50L);
+		usuario.setLogin("fhssouza@gmail.com");
+		usuario.setNome("Fábio Souza");
+		usuario.setSenha("123456");
+		
+		return ResponseEntity.ok(usuario);
+		
 	}
 
 }
